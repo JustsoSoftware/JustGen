40ceff8614c1434f5292948f1b4c3c314d1af088
jschirrmacher <j.schirrmacher@justso.de>
2015-08-08 20:52:53 +0200

    jslint

--- justtexts-plugin.js
+++ justtexts-plugin.js
@@ -5,22 +5,29 @@
  * @author     j.schirrmacher@justso.de
  */
 
-var pageEntry = $("#PageEntry"),
-    value = pageEntry.html();
+/* global jQuery, alert */
+/*jslint nomen: true */
 
-value = value.replace(/<\/span>/m, '</span><span class="template" contenteditable="true"><%= template %></span>');
-pageEntry.html(value);
+(function ($) {
+    "use strict";
+    var pageEntry = $("#PageEntry"),
+        value = pageEntry.html();
 
-$("#pages").find(".name").after('<span class="template">Template</span>');
+    value = value.replace(/<\/span>/m, '</span><span class="template" contenteditable="true"><%= template %></span>');
+    pageEntry.html(value);
 
-$(".navbar .container").append($('<button id="flush" class="pull-right btn">Flush Cache</button>'));
-$("#flush").on("click", function() {
-    $.get("/api/justgen/flushcache")
-        .then(function() {
-            window.router.refreshPageListView();
-        })
-        .fail(function(error, message) {
-            alert(message + ': ' + error.responseText);
-        });
-    return false;
-});
+    $("#pages").find(".name").after('<span class="template">Template</span>');
+
+    $(".navbar .container").append($('<button id="flush" class="pull-right btn">Flush Cache</button>'));
+    $("#flush").on("click", function () {
+        $.get("/api/justgen/flushcache")
+            .then(function () {
+                window.router.refreshPageListView();
+            })
+            .fail(function (error, message) {
+                alert(message + ': ' + error.responseText);
+            });
+        return false;
+    });
+
+}(jQuery));
