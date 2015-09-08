d259a03a8fe72ba3de6d11ee7d16bb05dd14d4a2
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2015-09-08 10:00:50 +0200

    Adapted to new interface of page processors

--- test/Processor.php
+++ test/Processor.php
@@ -11,7 +11,7 @@ class testTemplate implements \justso\justgen\ProcessorInterface
     {
     }
 
-    public function process($html)
+    public function process($html, $pageName)
     {
         return "Processed";
     }
